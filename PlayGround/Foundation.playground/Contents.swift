0efaf2f040ae5a9a19e4eb894d081034f9f7cdc8
xiaobing5 <xiaobing5@staff.sina.com.cn>
2020-11-05 20:07:30 +0800

    添加运算demo

---
+++ PlayGround/Foundation.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var numberA = 10
+var numberB = 20
+
+let numberC = numberA + numberB
+
+
+
+
+print("A + B 结果为：\(numberA + numberB)")
+print("A - B 结果为：\(numberA - numberB)")
+print("A * B 结果为：\(numberA * numberB)")
+print("B / A 结果为：\(numberB / numberA)")
+numberA += 1   // 类似 A++
+print("A += 1 后 A 的值为 \(numberA)")
+numberB -= 1   // 类似 B--
+print("B -= 1 后 B 的值为 \(numberB)")
